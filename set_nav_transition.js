9bce706ddd1e4b5403c5be0cb2f2ced744ef4ee2
acstAcMac <natnstart@gmail.com>
2022-09-23 16:37:53 +0800

    optimizing <400px nav hover

--- set_nav_transition.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {
-  let dropdowns = document.querySelectorAll("header nav ul li ul.dropdown");
-  dropdowns.forEach((dropdown) => {
-    dropdown.style.transition = "all 0.3s ease";
-  });
-}, 0);
